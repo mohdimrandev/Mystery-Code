7a277b1af3e8f76eb784ba8be6304d4262531716
Imran <imranmohd2209@gmail.com>
2024-05-29 23:33:15 +0530

    added a funtionality to get input and validate the user's guess

--- mystery_code.py
+++ mystery_code.py
@@ -14,3 +14,30 @@ def generate_code():
         code.append(color)
 
     return code
+
+
+def guess_code():
+    while True:
+        guess = input("Guess: ").upper().strip()
+        print(guess)
+
+        if guess.count(" ") != CODE_LENGTH - 1:
+            print("You must add space between each code. Try again")
+            continue
+
+        else:
+            guess = guess.split(" ")
+
+        if len(guess) != CODE_LENGTH:
+            print(f"You must guess {CODE_LENGTH} colors!")
+            continue
+
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid color: {color}. Try again!")
+                break
+
+        else:
+            break
+
+    return guess
