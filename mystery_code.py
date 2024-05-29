a3bbfc12e5353deaf5a6cc3117cba549f78d0a9b
Imran <imranmohd2209@gmail.com>
2024-05-29 22:53:51 +0530

    added a functionality to generate a 4 colour random code

---
+++ mystery_code.py
@@ -0,0 +1,16 @@
+import random
+
+COLORS = ["R", "G", "B", "Y", "W", "O"]
+TRIES = 10
+CODE_LENGTH = 4
+
+
+def generate_code():
+    code = []
+
+    for _ in range(CODE_LENGTH):
+        color = random.choice(COLORS)
+
+        code.append(color)
+
+    return code
