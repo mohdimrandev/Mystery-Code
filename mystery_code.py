4e5c2bb29df8e5ce50f324921f51ee0fbb6f3970
Imran <imranmohd2209@gmail.com>
2024-05-30 18:40:59 +0530

    added a functionality to tie all the game components together and create a game logic

--- mystery_code.py
+++ mystery_code.py
@@ -63,3 +63,27 @@ def check_code(guess, real_code):
             color_counts[guess_color] -= 1
 
     return correct_pos, incorrect_pos
+
+
+def game():
+    print(f"Welcome to Mystery Code game, you have {TRIES} to guess the code....")
+    print("The valid colors are", *COLORS)
+
+    code = generate_code()
+    for attempts in range(1, TRIES + 1):
+        guess = guess_code()
+        correct_pos, incorrect_pos = check_code(guess, code)
+
+        if correct_pos == CODE_LENGTH:
+            print("Congratulations, you won the game!")
+            print(f"You guessed the code in {attempts} attempts!")
+            break
+
+        print(f"Correct Positions: {correct_pos} | Incorrect Positions: {incorrect_pos}")
+
+    else:
+        print("You ran out of tries, the code was:", *code)
+
+
+if __name__ == "__main__":
+    game()
